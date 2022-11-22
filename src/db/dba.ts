776f1ba608baf7d71b4e561eef9cce3b520c7ce9
Cristian Zayas <chriss.zayas@gmail.com>
2022-11-21 18:51:42 -0800

    Dist/production

--- src/db/dba.ts
+++ src/db/dba.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const mongodb = () => {
-    mongoose.connect(`${process.env.CONNECTION}`)
+    mongoose.connect(`mongodb+srv://cristianzayas:YPtZYR5KXXhEF2Jj@cluster0.hquavtz.mongodb.net/?retryWrites=true&w=majority`)
         .then(result => console.log(`This run dba in mongodb`))
         .catch(error => console.log(`Sorry but not function server connection whit the dba`));
 }
